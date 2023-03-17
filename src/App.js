302b02053fa0a58c0d34ea108eb580f19684ba0f
PKFireBarry <Barry0719@gmail.com>
2023-03-16 21:02:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,23 +16,18 @@ import Faq from './components/Faq';
 
 function App() {
   return (
-    <div className="App scroll-smooth bg-slate-200">
+    <div className="App bg-slate-200 snap-y snap-mandatory overflow-x-hidden scroll-smooth">
       
 
       <NavBar/>
-      <Hero/>
-      <About/>
-      <Pricing/>
-      <Review/>
-      <ContactUs/>            
-      <Services/>
-      <Ethics/>
-      <Faq/>
-
-
-
-
-
+      <Hero className='snap-center '/>
+      <About className='snap-center '/>
+      <Pricing className='snap-center '/>
+      <Review className='snap-center '/>
+      <ContactUs className='snap-center '/>            
+      <Services className='snap-center '/>
+      <Ethics className='snap-center '/>
+      <Faq className='snap-center '/>
       <Footer/>
    </div>
   );
